88e4cbefed111dc34af645f08c44167f89c25666
Eduard <jimlab@mail.ru>
2020-12-26 16:38:55 +0300

    dist commit

---
+++ precache-manifest.0f139724d5b4b4f416c71cd409f8cbb7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a816006cd90fb63ec70",
+    "url": "/Vue_crm/css/app.d0e95c5f.css"
+  },
+  {
+    "revision": "09df8244a560104b469b",
+    "url": "/Vue_crm/css/chunk-0be56fc6.6076d0be.css"
+  },
+  {
+    "revision": "ac54c4dae8bb1c4650f8",
+    "url": "/Vue_crm/css/chunk-vendors.2955f2db.css"
+  },
+  {
+    "revision": "89e62a1dfd567f55a7436e5adaeb41cd",
+    "url": "/Vue_crm/index.html"
+  },
+  {
+    "revision": "6a816006cd90fb63ec70",
+    "url": "/Vue_crm/js/app.4d751db3.js"
+  },
+  {
+    "revision": "09df8244a560104b469b",
+    "url": "/Vue_crm/js/chunk-0be56fc6.3b8753f1.js"
+  },
+  {
+    "revision": "a9168ad51bb4485d3739",
+    "url": "/Vue_crm/js/chunk-2d0d7699.17cac909.js"
+  },
+  {
+    "revision": "046c4a5aeb8c4521c234",
+    "url": "/Vue_crm/js/chunk-2d21a3d2.ed999dc6.js"
+  },
+  {
+    "revision": "b75077adb7985b28be05",
+    "url": "/Vue_crm/js/chunk-3679971f.81f9fa45.js"
+  },
+  {
+    "revision": "ab2fe0b683f2fd376fcb",
+    "url": "/Vue_crm/js/chunk-5f4ad635.4296919f.js"
+  },
+  {
+    "revision": "66ffd787158b3cd2f24a",
+    "url": "/Vue_crm/js/chunk-63a63126.7757d742.js"
+  },
+  {
+    "revision": "8784a336b1aacc684cd9",
+    "url": "/Vue_crm/js/chunk-72449522.7a980de4.js"
+  },
+  {
+    "revision": "4d456790d8bc4fdae6b6",
+    "url": "/Vue_crm/js/chunk-73336dda.d8f000fc.js"
+  },
+  {
+    "revision": "62e83ab7852b7f4ec995",
+    "url": "/Vue_crm/js/chunk-c420df12.f3d01355.js"
+  },
+  {
+    "revision": "ac54c4dae8bb1c4650f8",
+    "url": "/Vue_crm/js/chunk-vendors.59957870.js"
+  },
+  {
+    "revision": "481e1118a004b375d927acafff9d37de",
+    "url": "/Vue_crm/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Vue_crm/robots.txt"
+  }
+]);
